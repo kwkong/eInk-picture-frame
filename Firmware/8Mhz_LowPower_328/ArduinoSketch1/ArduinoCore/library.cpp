89b150fbc122834f60a506592b3781a33495d1a3
Kriston Kong <kristonkong@gmail.com>
2021-03-18 14:26:17 -0500

    updates

---
+++ Firmware/8Mhz_LowPower_328/ArduinoSketch1/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 7/3/2021 11:03:27
+ * Author : krist
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ Firmware/8Mhz_LowPower_328/ArduinoSketch1/sketch_mar07a/Sketch.cpp
@@ -0,0 +1,34 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+
+/*End of auto generated code by Atmel studio */
+
+#include <Arduino.h>
+#include <LowPower.h>
+#include <SoftwareSerial.h>
+//Beginning of Auto generated function prototypes by Atmel Studio
+//End of Auto generated function prototypes by Atmel Studio
+
+#define F_CPU 8000000
+
+unsigned long rxTimeout = 0;
+//SoftwareSerial loraSerial(2,3);
+String message;
+
+void setup()
+{
+  //pinMode(4, OUTPUT);
+  //digitalWrite(4, HIGH);
+  //delay(1000);
+  //loraSerial.begin(9600);
+  Serial.begin(9600);
+  Serial.println("init");
+  delay(1000);
+}
+
+void loop()
+{
+	Serial.println("Going to sleep now");
+	delay(25);
+	LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); 
+}
