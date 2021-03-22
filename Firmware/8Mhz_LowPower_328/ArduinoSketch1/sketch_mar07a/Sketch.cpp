4913ff431d3dd6e1d1ee6fb7de572271722dbb2c
kwkong <kristonkong@gmail.com>
2021-03-21 23:23:06 -0500

    updates

--- Firmware/8Mhz_LowPower_328/ArduinoSketch1/sketch_mar07a/Sketch.cpp
+++ Firmware/8Mhz_LowPower_328/ArduinoSketch1/sketch_mar07a/Sketch.cpp
@@ -6,12 +6,14 @@
 #include <LowPower.h>
 #include <SoftwareSerial.h>
 
-#define F_CPU 8000000
 #define eink_EN 4
 
 unsigned long rxTimeout = 0;
 //SoftwareSerial loraSerial(2,3);
 String message;
+
+const int wake_up = 2;
+const int reset = 3;
 
 void setup()
 {
@@ -19,30 +21,57 @@ void setup()
   //digitalWrite(4, HIGH);
   //delay(1000);
   //loraSerial.begin(9600);
-  Serial.begin(9600);
-  Serial.println("init");
+  //Serial.begin(9600);
+  //Serial.println("init");
  
   pinMode(eink_EN, OUTPUT);
   
-  delay(1000);
+  delay(2000);
   digitalWrite(eink_EN, HIGH);
-  delay(1000);
+  delay(4000);
   
+		Serial.begin(57600);
+      //epd_init(wake_up, reset);
+		delay(1000);
+      epd_wakeup(wake_up);
+	  delay(1000);
+      epd_set_memory(MEM_NAND);
+      delay(1000);
+      //epd_clear();
+      delay(2000);
   
-    epd_clear();
-    epd_disp_bitmap("PIC4.BMP", 0, 0);
-    epd_udpate();
-    delay(5000);
+	//   char buff[] = {'G', 'B', 'K', '3', '2', ':', ' ', 0xc4, 0xe3, 0xba, 0xc3, 0xca, 0xc0, 0xbd, 0xe7, 0x00};
+	epd_set_color(BLACK, WHITE);
+	delay(1000);
+	//   epd_set_ch_font(GBK32);
+	//   epd_set_en_font(ASCII32);
+	//   epd_disp_string(buff, 0, 50);
+	//   epd_disp_string("ASCII32: Hello, World", 0, 300);
 
-    epd_clear();
-    epd_disp_bitmap("PIC2.BMP", 0, 100);
-    epd_disp_bitmap("PIC3.BMP", 400, 100);
-    epd_udpate();
-    delay(5000);
+	//   epd_set_ch_font(GBK48);
+	//   epd_set_en_font(ASCII48);
+	//   buff[3] = '4';
+	//   buff[4] = '8';
+	//   epd_disp_string(buff, 0, 100);
+	//   epd_disp_string("ASCII48: Hello, World", 0, 350);
 
-    epd_clear();
-    epd_disp_bitmap("PIC7.BMP", 0, 0);
-    epd_udpate();
+	epd_set_ch_font(GBK64);
+	delay(1000);
+	epd_set_en_font(ASCII64);
+	delay(1000);
+	//   buff[3] = '6';
+	//   buff[4] = '4';
+	//   epd_disp_string(buff, 0, 160);
+	epd_disp_string("JANICE IS BIG DUMB", 190, 200);
+	delay(1000);
+	epd_udpate();
+	delay(3000);
+	epd_clear();
+	delay(3000);
+	epd_disp_string("JK SHE AIIGHT", 250, 200);
+	epd_udpate();
+
+	delay(3000);
   
   
   delay(1000);
@@ -52,7 +81,7 @@ void setup()
 
 void loop()
 {
-	Serial.println("Going to sleep now");
-	delay(500);
+	//Serial.println("Going to sleep now");
+	//delay(500);
 	//LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); 
 }
